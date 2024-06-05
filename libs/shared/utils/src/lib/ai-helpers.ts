325b467eaeda88826822d8fb361df79051fd5496
Richard Vance <girthub@13d.org>
2024-06-05 18:48:27 -0500

    remove debugging

--- libs/shared/utils/src/lib/ai-helpers.ts
+++ libs/shared/utils/src/lib/ai-helpers.ts
@@ -52,7 +52,6 @@ const processEnvSecrets = JSON.parse(process.env['SECRETS'] || process.env['secr
 
 export const analyze = async (imagePrompt: string) => {
   const apiKey = processEnvSecrets['OPENAI_API_KEY'] || process.env['OPENAI_API_KEY'];
-  console.log('oai', apiKey, 'e', JSON.stringify(process.env));
   const input = await getPrompt(imagePrompt);
   const model = new OpenAI({
     temperature: 0.6,
