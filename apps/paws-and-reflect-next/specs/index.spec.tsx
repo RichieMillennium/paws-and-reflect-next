b3bb51185710eaad4de048ad5e433e5e7c8d914b
Richard Vance <girthub@13d.org>
2024-06-05 19:10:47 -0500

    remove debugging and update README

--- apps/paws-and-reflect-next/specs/index.spec.tsx
+++ apps/paws-and-reflect-next/specs/index.spec.tsx
@@ -1,6 +1,33 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 
+jest.mock('@langchain/openai', () => ({
+  OpenAI: class {
+    invoke() {
+      return 'test';
+    }
+  }
+}));
+
+jest.mock('@langchain/core/output_parsers', () => ({
+  StructuredOutputParser: {
+    fromZodSchema: () => {
+      return {
+        getFormatInstructions: jest.fn(),
+        parse: jest.fn(),
+      }
+    }
+  }
+}));
+
+jest.mock('@langchain/core/prompts', () => ({
+  PromptTemplate: class {
+    format() {
+      return Promise.resolve(null);
+    }
+  }
+}));
+
 import Page from '../app/page';
 
 describe('Page', () => {
